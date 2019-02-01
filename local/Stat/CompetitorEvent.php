b6202454f97bac5e755b8f35f3257d4dd2863eec
Igor Achkasov <a.i.n.1989@mail.ru>
2019-02-01 10:21:24 +0300

    fix(Статистика) удалился главный якорь

--- local/Stat/CompetitorEvent.php
+++ local/Stat/CompetitorEvent.php
@@ -14,7 +14,7 @@ final class CompetitorEvent {
 
   private const CREATE_EVENT = "INSERT INTO bf_stat_event (DEAL_ID, DATA, EVENT, LINK) VALUES(%d, '%s', '%s', '%s')";
 
-  public function dispatch(int $deal_id, array &$data) : bool {
+  public function dispatch(int $deal_id, array &$data, bool $is_main_anchor = false) : bool {
 
     if(!$this->exists($deal_id)) {
 
@@ -28,6 +28,12 @@ final class CompetitorEvent {
 
     $dataIds    = array_column($data, 'ID');
 
+    if($is_main_anchor) {
+
+       return $this->mainDelete($deal_id);
+
+    }
+
     if($this->allDelete($deal_id, $data)) {
 
        return true;
@@ -38,6 +44,16 @@ final class CompetitorEvent {
     $this->oneDelete($deal_id, $currentIds, $dataIds, $currentData);
 
     return true;
+
+  }
+
+  private function mainDelete(int $deal_id) : bool {
+
+    $deal = \CCrmDeal::GetList(['ID' => 'DESC'], ['CHECK_PERMISSIONS' => 'N', 'ID' => $deal_id], ['UF_CRM_1542029126']);
+
+    $data = json_decode($deal->Fetch()['UF_CRM_1542029126'], 1);
+
+    return $this->create($deal_id, $data, 'MAIN_DELETE');  
 
   }
 
