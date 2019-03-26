f3a570360d6bf8befa6e8c645b34ccfe21e36597
Igor Achkasov <a.i.n.1989@mail.ru>
2019-03-26 15:59:11 +0300

    fix(Автотексты) порядок полей

--- local/XML/Helpers/Description.php
+++ local/XML/Helpers/Description.php
@@ -10,206 +10,197 @@ trait Description {
 
   private static $OZS = 76;
 
-  protected function getDescription(int $category, array &$semantic, array &$arFields) : string {
+  protected function getDescription(int $category, array &$semantics, array &$arFields) : string {
 
     $path = SemanticFactory::create($category);
 
     $arSemantic = $this->loadSemantic($path);
 
-    $semantic_code = self::SEMANTIC_CODE[$category];
-
     $auto_text = '';
 
-     /**
-     * Cемантика
-     */
-    foreach($semantic as $value) {
-    
-      if(is_array($arSemantic[$value]) && array_key_exists("LOGIC", $arSemantic[$value])) {
+    foreach($arSemantic as $code => $semantic) {
 
-        $logics = $arSemantic[$value]['LOGIC'];
+      /**
+       * поля раздела семантики
+       */
+      if(is_numeric($code) && in_array($code, $semantics)) {
 
-        foreach($logics as $logic) {
+        #echo $code,'<br>';
 
-          foreach($logic as $field => $condition) {
+        if(is_array($arSemantic[$code]) && array_key_exists("LOGIC", $arSemantic[$code])) {
 
-            if(in_array($field, $semantic) !== false) {
-
-              #echo $index,' ',$field,' - <br>';
-
-              $text = $this->parse($arSemantic[$value]['TEXT'], $arFields);
-
-              $auto_text.= $text;
-
-              if($text) {
-
-                $auto_text.= ',';
-
-             }
-          
-           } 
-         }
-       }
-      } else {
-
-      if($value == 322) {
-
-        if(strpos($arFields['UF_CRM_1544431330'],'лет') !== false) {
-
-           $year = (int)$arFields['UF_CRM_1544431330'];
-
-           if($year > 8) {
-
-              echo 'пропуск';
-
-              continue;
-
+          $logics = $arSemantic[$code]['LOGIC'];
+  
+          foreach($logics as $logic) {
+  
+            foreach($logic as $field => $condition) {
+  
+              if(in_array($field, $semantic) !== false) {
+  
+                #echo $index,' ',$field,' - <br>';
+  
+                $text = $this->parse($arSemantic[$code]['TEXT'], $arFields);
+  
+                $auto_text.= $text;
+  
+                if($text) {
+  
+                  $auto_text.= ',';
+  
+               }
+            
+             } 
            }
-
-
+         }
+        } else {
+  
+        if($code == 322) {
+  
+          if(strpos($arFields['UF_CRM_1544431330'],'лет') !== false) {
+  
+             $year = (int)$arFields['UF_CRM_1544431330'];
+  
+             if($year > 8) {
+  
+               // echo 'пропуск';
+  
+                continue;
+  
+             }
+          }
+  
         }
+  
+        #echo $value,'<br>';
+  
+        $text  = $this->parse($arSemantic[$code], $arFields);
+  
+        if($text) {
+  
+           $auto_text.= $text;
+           $auto_text.= ',';
+  
+        }
+       }
 
-      }
 
-      #echo $value,'<br>';
-
-      $text  = $this->parse($arSemantic[$value], $arFields);
-
-      if($text) {
-
-         $auto_text.= $text;
-         $auto_text.= ',';
-
-      }
-     }
-    }
-
-    /**
-    * Остальные поля
-    */
-
-    foreach($arSemantic as $code=>$value) {
-
-      if(array_key_exists($code, $arFields)) {
-
+      } elseif(array_key_exists($code, $arFields))  {
+      /**
+       * остальные поля
+       *  
+       */
 
         if($arFields['UF_CRM_1540371261836'] != self::$OZS && $code == 'UF_CRM_1540202667') {
 
-          #echo $arFields['UF_CRM_1540371261836'],'<br>';
-
-          continue; 
+           continue; 
 
         } 
-        
-        if($arFields['UF_CRM_1540371938'] == 0 && $arFields['UF_CRM_1540371261836'] != self::$OZS
       
-                 && $code == 'UF_CRM_1540371938') {
+        if($arFields['UF_CRM_1540371938'] == 0 && $arFields['UF_CRM_1540371261836'] != self::$OZS
+    
+           && $code == 'UF_CRM_1540371938') {
 
-          continue; 
-        
+           continue; 
+      
         }
 
-         /**
-          *  множественное
-          */
+  
+        if(is_array($arSemantic[$code])) {
 
-         if(is_array($arSemantic[$code])) {
+          $multi_text = $arSemantic[$code];
 
-            $multi_text = $arSemantic[$code];
+          foreach($multi_text as $index => $text) {
 
-            foreach($multi_text as $index => $text) {
+             #echo $index,' ', $text, '<br>';
+             #echo $arFields[$index],'<br>';
 
-               #echo $index,' ', $text, '<br>';
-               #echo $arFields[$index],'<br>';
+             if($index  == 'UF_CRM_1543406565') {
 
-               if($index  == 'UF_CRM_1543406565') {
+                $row_value = iblockValue($arFields[$index]);
 
-                  $row_value = iblockValue($arFields[$index]);
-
-                    #echo  $row_value,'<br>';
-
-                } else {
-                  
-                  if($index == 'STREET' || $index == 'PLACE') {
-
-                      #echo $index,' ', print_r($multi_text[$index],1);
-
-                    if($arFields['UF_CRM_1540202889'] == self::STREET_TYPE) {
-
-                        foreach($multi_text['STREET'] as $key=>$location) {
-
-                          #print_r([$key,$location]);
-
-                          $row_value = enumValue((int)$arFields[$key], $key) ? : $arFields[$key];
-
-                          #echo $key,' ',$row_value,'<br>';
-
-                          $auto_text.= str_replace($key, $row_value , $location);
-
-                          $auto_text.= ',';
-
-                        }
-
-                     } else {
-
-                      foreach($multi_text['PLACE'] as $key=>$location) {
-
-                         #print_r([$key,$location]);
-
-                         $row_value = enumValue((int)$arFields[$key], $key) ? : $arFields[$key];
-
-                         #echo $key,' ',$row_value,'<br>';
-
-                         $auto_text.= str_replace($key, $row_value , $location);
-
-                         $auto_text.= ',';
-
-                      }
-                     }
-
-                  } else {
-
-                    $row_value = enumValue((int)$arFields[$index], $index) ? : $arFields[$index];
-                    $auto_text.= str_replace($index, $row_value , $text);
-                    $auto_text.= ',';
-
-                }
-              }
-            }
-          } else {
-
-            if(is_array($arFields[$code])) {
-
-               // echo $code,'<br>';
-
-               $auto_text.= $value;
-               $auto_text.= implode(',', array_map(function($item) use($code) {
-                
-                 return enumValue($item, $code);
-              
-               }, $arFields[$code]));
-               
-
-             } else {
-
-              $text_value = enumValue($arFields[$code], $code) ? : $arFields[$code];
-
-              if(strpos($value, $code) !== false) {
-
-                 $auto_text.= str_replace($code, $text_value, $value);
+                  #echo  $row_value,'<br>';
 
               } else {
+                
+                if($index == 'STREET' || $index == 'PLACE') {
 
-                $auto_text.= $value;
+                    #echo $index,' ', print_r($multi_text[$index],1);
+
+                  if($arFields['UF_CRM_1540202889'] == self::STREET_TYPE && $index == 'STREET') {
+
+                      foreach($multi_text['STREET'] as $key=>$location) {
+
+                        #print_r([$key,$location]);
+
+                        $row_value = enumValue((int)$arFields[$key], $key) ? : $arFields[$key];
+
+                        #echo $key,' ',$row_value,'<br>';
+
+                        $auto_text.= str_replace($key, $row_value , $location);
+
+                        $auto_text.= ',';
+
+                      }
+
+                   } elseif($index == 'PLACE') {
+
+                    foreach($multi_text['PLACE'] as $key=>$location) {
+
+                       #print_r([$key,$location]);
+
+                       $row_value = enumValue((int)$arFields[$key], $key) ? : $arFields[$key];
+
+                       #echo $key,' ',$row_value,'<br>';
+
+                       $auto_text.= str_replace($key, $row_value, $location);
+
+                       $auto_text.= ',';
+
+                    }
+                   }
+
+                } else {
+
+                  $row_value = enumValue((int)$arFields[$index], $index) ? : $arFields[$index];
+                  $auto_text.= str_replace($index, $row_value , $text);
+                  $auto_text.= ',';
 
               }
-                
-              $auto_text.= ',';
+            }
+          }
+        } else {
+
+          if(is_array($arFields[$code])) {
+
+             $auto_text.= $arSemantic[$code];
+             $auto_text.= implode(',', array_map(function($item) use($code) {
+              
+               return enumValue($item, $code);
+            
+             }, $arFields[$code]));
+             
+
+           } else {
+
+            $text_value = enumValue($arFields[$code], $code) ? : $arFields[$code];
+
+            if(strpos($value, $code) !== false) {
+
+               $auto_text.= str_replace($code, $text_value, $value);
+
+            } else {
+
+              $auto_text.= $value;
 
             }
+              
+            $auto_text.= ',';
+
+          }
         }
       }
    }
+  
 
    return substr($auto_text,0,-1);
 
