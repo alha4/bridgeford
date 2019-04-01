c945167560502aec51d83d804a61337f8c150902
Igor Achkasov <a.i.n.1989@mail.ru>
2019-04-01 12:00:59 +0300

    fix(Автотекст) исправленая версия

--- local/XML/Helpers/Description.php
+++ local/XML/Helpers/Description.php
@@ -35,12 +35,12 @@ trait Description {
       /**
        * поля раздела семантики
        */
-      if(is_numeric($code) && in_array($code, $semantics)) {
+     if(is_numeric($code) && in_array($code, $semantics)) {
 
         /**
          * поле семантики с логикой
          */
-        if(is_array($arSemantic[$code]) && array_key_exists("LOGIC", $arSemantic[$code])) {
+       if(is_array($arSemantic[$code]) && array_key_exists("LOGIC", $arSemantic[$code])) {
 
           $logics = $arSemantic[$code]['LOGIC'];
   
@@ -50,48 +50,44 @@ trait Description {
   
               if(in_array($field, $semantic) !== false) {
   
-                $text = $this->parse($arSemantic[$code]['TEXT'], $arFields);
+                $logic_text = $this->parse($arSemantic[$code]['TEXT'], $arFields);
   
-                $auto_text.= $text;
-  
-                if($text) {
+                if($logic_text) {
+                   $auto_text.= $logic_text;
                    $auto_text.= ',';
                 }
-
              } 
            }
          }
-        } else {
+       } else {
         
         /**
          * 
          * Высоколиквидный объект
          * UF_CRM_1544431330 - окупаемость
          */
-        if($code == self::$HIGHLY_LIQUID_OBJECT) {
+       if($code == self::$HIGHLY_LIQUID_OBJECT) {
   
-          if(strpos($arFields['UF_CRM_1544431330'],'лет') !== false) {
+         if(strpos($arFields['UF_CRM_1544431330'],'лет') !== false) {
   
-             $year = (int)$arFields['UF_CRM_1544431330'];
+            $year = (int)$arFields['UF_CRM_1544431330'];
   
-             if($year > self::$LEAST_YEAR) {
+            if($year > self::$LEAST_YEAR) {
   
                 continue;
   
-             }
+            }
           }
-  
+       }
+
+       $semantic_text = $this->parse($arSemantic[$code], $arFields);
+
+       if($semantic_text) {
+
+          $auto_text.= $semantic_text;
+          $auto_text.= ',';
+
         }
-
-        $text = $this->parse($arSemantic[$code], $arFields);
-        $auto_text.= $text;
-
-        if($text) {
-
-           $auto_text.= ',';
-
-        }
-     
       } 
       /**
       * остальные польз-е поля (не из семантики) 
@@ -106,34 +102,34 @@ trait Description {
          $type = false;
 
          if($arFields['UF_CRM_1540371261836'] == self::$OZS && $code == 'UF_CRM_1540202667' && 
-             $arFields['UF_CRM_1540371938'] == 0) {
+            $arFields['UF_CRM_1540371938'] == 0) {
 
       
-              $code = 'UF_CRM_1540202667';
+            $code = 'UF_CRM_1540202667';
 
-              $type = 'ОЗС';
+            $type = 'ОЗС';
 
          } elseif($arFields['UF_CRM_1540371261836'] == self::$OZS && $code == 'UF_CRM_1540371938' &&
-             $arFields['UF_CRM_1540371938'] == 1) {
+           $arFields['UF_CRM_1540371938'] == 1) {
 
-              $code = 'UF_CRM_1540371938';
+           $code = 'UF_CRM_1540371938';
 
-              $type = 'ОЗС + особняк';
+           $type = 'ОЗС + особняк';
 
            
          } elseif($arFields['UF_CRM_1540371261836'] != self::$OZS && $code == 'UF_CRM_1540371455' &&
-             $arFields['UF_CRM_1540371938'] == 0 && $arFields['UF_CRM_1540371455'] == self::$NEW_BUILDING) {
+           $arFields['UF_CRM_1540371938'] == 0 && $arFields['UF_CRM_1540371455'] == self::$NEW_BUILDING) {
 
-              $code == 'UF_CRM_1540371455';
+           $code == 'UF_CRM_1540371455';
 
-              $type = 'Новостройка';
+           $type = 'Новостройка';
             
          } elseif($arFields['UF_CRM_1540371261836'] != self::$OZS && $code == 'UF_CRM_1540384807664' &&
-              $arFields['UF_CRM_1540371938'] == 0 && $arFields['UF_CRM_1540371455'] != self::$NEW_BUILDING) {
+           $arFields['UF_CRM_1540371938'] == 0 && $arFields['UF_CRM_1540371455'] != self::$NEW_BUILDING) {
 
-              $code = 'UF_CRM_1540384807664';
+           $code = 'UF_CRM_1540384807664';
 
-              $type = 'Тип здания';
+           $type = 'Тип здания';
 
          } 
 
@@ -143,77 +139,75 @@ trait Description {
 
          } 
 
-        $multi_text = $arSemantic[$code];
+         $arText = $arSemantic[$code];
 
-        foreach($multi_text as $index => $text) {
+         foreach($arText as $index => $milti_text) {
 
           if($index == self::$METRO) {
 
-               $row_value = iblockValue($arFields[$index]);
+            $row_value = iblockValue($arFields[$index]);
 
-           } else {
+            $auto_text.= str_replace($index, $row_value, $milti_text);
+            $auto_text.= ',';
+
+          } else {
                 
             if($arFields['UF_CRM_1540202889'] == self::STREET_TYPE && $index == 'STREET') {
 
-                 foreach($multi_text['STREET'] as $key=>$location) {
+              foreach($arText['STREET'] as $key=>$location) {
 
-                     $row_value = enumValue((int)$arFields[$key], $key) ? : $arFields[$key];
-                     $auto_text.= str_replace($key, $row_value , $location);
+                $row_value = enumValue((int)$arFields[$key], $key) ? : $arFields[$key];
+                $auto_text.= str_replace($key, $row_value , $location);
              
-
-                  }
-
-                  if($text) {
-
-                    $auto_text.= ',';
-
-                 }
-                 
-
-              } elseif($arFields['UF_CRM_1540202889'] != self::STREET_TYPE && $index == 'PLACE') {
-
-                  foreach($multi_text['PLACE'] as $key=>$location) {
-
-                     $row_value = enumValue((int)$arFields[$key], $key) ? : $arFields[$key];
-                     $auto_text.= str_replace($key, $row_value, $location);
-
-                  }
-            
-                  if($text) {
-
-                     $auto_text.= ',';
-
-                  }
-                
-              } else {
-
-                 if($arFields['UF_CRM_1540202667'] == self::$MOSKOW && $index == 'UF_CRM_1540202817') {
-
-                     continue;
-
-                 }
-
-                 if($arFields['UF_CRM_1540202667'] != self::$MOSKOW && $index == 'UF_CRM_1540203111') {
-
-                    continue;
-
-                 }
-
-                 // тут проблема
-                 $row_value = enumValue((int)$arFields[$index], $index) ? : $arFields[$index];
-
-                 $auto_text.= str_replace($index, $this->regionMorphology($row_value), $text);
-
-              
-                 if($text) {
-
-                     $auto_text.= ',';
-
-                 }
               }
-            } 
-          }
-        } else {
+
+              $auto_text.= ',';
+     
+            } elseif($arFields['UF_CRM_1540202889'] != self::STREET_TYPE && $index == 'PLACE') {
+
+              foreach($arText['PLACE'] as $key=>$location) {
+
+                $row_value = enumValue((int)$arFields[$key], $key) ? : $arFields[$key];
+                $auto_text.= str_replace($key, $row_value, $location);
+
+              }
+
+              $auto_text.= ',';
+               
+            } else {
+
+              /**
+               * если регион москва, город не выводим
+               */
+
+              if($arFields['UF_CRM_1540202667'] == self::$MOSKOW && $index == 'UF_CRM_1540202817') {
+
+                continue;
+
+              }
+
+              /**
+               * если регион не москва, округ не выводим
+               */
+
+              if($arFields['UF_CRM_1540202667'] != self::$MOSKOW && $index == 'UF_CRM_1540203111') {
+
+                  continue;
+
+              }
+
+              $row_value = enumValue((int)$arFields[$index], $index) ? : $arFields[$index];
+
+              if($row_value) {
+                
+                $auto_text.= str_replace($index, $this->regionMorphology($row_value), $milti_text);
+                $auto_text.= ',';
+
+              }
+            }
+          } 
+        }
+       } else {
 
         /**
          * множественное пользо-е поле 
@@ -225,37 +219,19 @@ trait Description {
 
                 return enumValue($item, $code);
 
-
              }, $arFields[$code]));
 
+             $auto_text.= ', ';
 
          } else {
 
-            if(is_array($text)) {
+          $text = $arSemantic[$code];
 
+          $text_value = enumValue((int)$arFields[$code], $code) ? : $arFields[$code];
 
-              $auto_text_emum = implode(',', array_map(function($item) use($arFields,$code) {
+          if($text_value) {
 
-
-                $text_value = enumValue((int)$arFields[$code], $code) ? : $arFields[$code];
-
-                #echo $code,', ',$text_value,'<br>';
-
-                return str_replace($key, $text_value, $item);
-
-
-              }, $text));
-
-
-              continue;
-
-            }
-
-            $text_value = enumValue((int)$arFields[$code], $code) ? : $arFields[$code];
-
-            #echo $text,' ,',$text_value,'<br>';
-
-             //тут проблема 
+              #echo $code,' ',$arFields['ID'],' ',  $text,' ,',$text_value,'<br>';
 
              if(strpos($text, $code) !== false) {
  
@@ -263,15 +239,18 @@ trait Description {
  
              } else {
  
-               $auto_text.= $text;
- 
-             }
+                $auto_text.= $text;
+             } 
 
-         
-          } 
-        }
-        
-      }
+             if($text_value) {
+
+                $auto_text.= ',';
+
+             }
+          }
+        } 
+      }   
+    }
    }
   
    return substr($auto_text,0,-1);
